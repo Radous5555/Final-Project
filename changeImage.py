a9f18fcab490c898f9b25602eb40f5e93485a49a
Radovan <radovan.opocensky@gamil.com>
2024-08-20 12:48:32 +0200

    Added body of the script, which should find the tif images, resize, convert and save them as jpgs depending on the operating system.
    Can be improved with a better file identifying method.
    
    Should break if directory does not exist.

--- changeImage.py
+++ changeImage.py
@@ -2,5 +2,15 @@
 
 from PIL import Image
 import os
+import platform
 
-directory = os.getcwd() + "/supplier-data/images"
+directory = os.getcwd() + "/supplier-data/images/"
+
+for image in os.listdir(directory):
+    if image.endswith(".tif") or image.endswith(".tiff"):
+        name, extension = os.path.splitext(image)
+        old_img = Image.open(directory + image)
+        if platform.system() == "Windows":
+            old_img.resize((600,400)).convert("RGB").save(directory + name + ".jpeg", "jpeg")
+        else:
+            old_img.resize((600,400)).convert("RGB").save(directory + name, "jpeg")
