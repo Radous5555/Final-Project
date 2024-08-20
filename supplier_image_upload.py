02c205c7bff0eb487376d12c895747db6ff4375a
Radovan <radovan.opocensky@gamil.com>
2024-08-20 13:31:58 +0200

    Wrote a script to send converted images from changeImage.py to the url, this will send all jpegs in the directory, regardless if they've been already uploaded.
    Can be improved with a different file identifying method.

---
+++ supplier_image_upload.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import requests
+import os
+
+directory = os.getcwd() + "/supplier-data/images/"
+
+""" paste the url here """
+url = "IP.ADDY.GOES.HERE/upload"
+
+for new_image in os.listdir(directory):
+    """ this will send all jpegs, regardless of whether or not they've already been uploaded """
+    if new_image.endswith(".jpeg"):
+        with open(directory + new_image, "rb") as opened:
+            sent = requests.post(url, files={"file": opened})
+            print(sent.status_code)
