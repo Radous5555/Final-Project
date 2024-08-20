4394c9dc32bdebc3f70a189497d0a62b8fa453b4
Radovan <radovan.opocensky@gamil.com>
2024-08-20 16:45:05 +0200

    Created a script calling the reports.py module to generate a pdf summarizing the data processed by run.py including the current date.

---
+++ report_email.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import datetime
+from datetime import date
+import reports
+
+descriptions = os.getcwd() + "/supplier-data/descriptions/"
+
+today = date.today()
+date = today.strftime("%B %d, %Y")
+
+paragraph = []
+for file in os.listdir(descriptions):
+    if file.endswith(".txt"):
+        with open (descriptions + file) as read_file:
+            lines = read_file.read().splitlines()
+            new_lines = ["name: " + lines[0] + "<br/>" + "weight: " + lines[1]]
+            paragraph.extend(new_lines)
+paragraph_joined = "<br/><br/>".join(paragraph)
+
+def main():
+    reports.generate(descriptions + "processed.pdf", "Processed Update on " + date, paragraph_joined)
+
+if __name__ == "__main__":
+    main()
