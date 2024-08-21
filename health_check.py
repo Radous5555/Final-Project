5d969912ad9e0e5a8909468f559b438f240304a5
Radovan <radovan.opocensky@gamil.com>
2024-08-21 16:05:32 +0200

    Added a very hideous but (hopefully) functional machine statistics check, will tidy up later.

---
+++ health_check.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import shutil
+import psutil
+import emails
+import socket
+
+mail_url = "IP.ADDY.GOES.HERE/webmail"
+sender = "automation@example.com"
+recipient = "student@example.com"
+body = "Please check your system and resolve the issue as soon as possible."
+
+def check_cpu_usage():
+    usage = psutil.cpu_percent(1)
+    return usage < 80
+
+def check_disk_usage():
+    disk_usage = shutil.disk_usage("/")
+    free_disk = disk_usage.free/disk_usage.total*100
+    return free_disk > 20
+
+def check_memory_usage():
+    mem = psutil.virtual_memory()
+    limit = 100 * 1024 * 1024  # 100MB
+    return mem.available > limit
+
+def check_localhost():
+    return socket.gethostbyname("localhost") == "127.0.0.1"
+    
+if not check_cpu_usage():
+    subject = "Error - CPU usage is over 80%"
+    message = emails.generate_error_report(sender, recipient, subject, body)
+    emails.send_email(message, mail_url)
+
+if not check_disk_usage():
+    subject = "Error - Available disk space is less than 20%"
+    message = emails.generate_error_report(sender, recipient, subject, body)
+    emails.send_email(message, mail_url)
+
+if not check_memory_usage():
+    subject = "Error - Available memory is less than 100MB"
+    message = emails.generate_error_report(sender, recipient, subject, body)
+    emails.send_email(message, mail_url)
+
+if not check_localhost():
+    subject = "Error - localhost cannot be resolved to 127.0.0.1"
+    message = emails.generate_error_report(sender, recipient, subject, body)
+    emails.send_email(message, mail_url)
